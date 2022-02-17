f3d1477f4484b270ef906fc3df7716ff67bcb00e
AOI0107 <gtmwaoi0810@gmail.com>
2022-02-17 10:55:51 +0900

    5-3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h3>My Todo</h3>
         <Route path="/" component={TodoList} exact={true} />>
         <Route path="/todo/add" component={TodoForm} />
       </div>
